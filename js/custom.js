d96f7d5f6911862d1e048a8338f5fb27fbbbb265
dkhan786-web <63242919+dkhan786-web@users.noreply.github.com>
2021-10-18 20:19:58 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,73 @@
 
-  $(function () {
+$(function () {
 
-    // MENU
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
-    });
-
-    // AOS ANIMATION
-    AOS.init({
-      disable: 'mobile',
-      duration: 800,
-      anchorPlacement: 'center-bottom'
-    });
-
-
-    // SMOOTHSCROLL NAVBAR
-    $(function() {
-      $('.navbar a, .hero-text a').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
-      });
-    });    
+  // AOS ANIMATION
+  AOS.init({
+    disable: 'mobile',
+    duration: 800,
+    anchorPlacement: 'center-bottom'
   });
 
 
-    
+  // SMOOTHSCROLL NAVBAR
+  $(function () {
+    $('.navbar a, .hero-text a').on('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
+    });
+  });
+});
 
+
+// Navbar Extra
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", () => {
+  navbar.classList.toggle("showInput");
+  if (navbar.classList.contains("showInput")) {
+    searchBox.classList.replace("bx-search", "bx-x");
+  } else {
+    searchBox.classList.replace("bx-x", "bx-search");
+  }
+});
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+  navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function () {
+  navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function () {
+  navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function () {
+  navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function () {
+  navLinks.classList.toggle("show3");
+}
+
+
+// Scroll down
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
